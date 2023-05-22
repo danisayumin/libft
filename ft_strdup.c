3d73f565cd073a6be850a3360eb671630ec886db
Daniela Sayumi nitta <dsayumi-@c1r8p2.42sp.org.br>
2023-05-22 19:03:40 -0300

    subindo calloc e strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dsayumi- <dsayumi-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 20:25:45 by dsayumi-          #+#    #+#             */
-/*   Updated: 2023/05/12 20:33:59 by dsayumi-         ###   ########.fr       */
+/*   Updated: 2023/05/22 19:01:27 by dsayumi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 
 char	*ft_strdup(const char *s)
 {
-    char *str;
-    size_t i;
+	char	*str;
+	size_t	i;
 
-    i = 0;
-    str = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
-    if (!str)
-        return (NULL);
-    while (s[i] != '\0')
-    {
-        str[i] = s[i];
-        i++;
+	i = 0;
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (!str)
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		str[i] = s[i];
+		i++;
     }
     str[i] = '\0';
-    return (str);
+	return (str);
 }
