c1971891307ccf59858ffcb494d6b3c283b934c6
Daniela Sayumi nitta <dsayumi-@c1r7p3.42sp.org.br>
2023-06-02 21:38:44 -0300

    finish slipt and need to fix is alpha and is digit

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: dsayumi- <dsayumi-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 19:52:59 by dsayumi-          #+#    #+#             */
-/*   Updated: 2023/05/29 21:47:33 by dsayumi-         ###   ########.fr       */
+/*   Updated: 2023/06/02 20:52:08 by dsayumi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static int	ft_count_word(const char *s, char c)
 	size_t	count;
 	size_t	i;
 
-	count = 1;
+	count = 0;
 	i = 0;
 	while (s[i])
 	{
@@ -33,14 +33,30 @@ static int	ft_count_word(const char *s, char c)
 	return (count);
 }
 
-static	int	ft_size_word(const char *s, char c)
+static void	ft_malloc_words(const char *s, char c, char **result)
 {
-	size_t	size;
+	size_t	substr;
+	size_t	i;
+	size_t	start;
 
-	size = 0;
-	while (s[size] != c && s[size])
-		size++;
-	return (size);
+	substr = 0;
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] != c)
+		{
+			start = i;
+			while (s[i] != c && s[i] != '\0')
+				i++;
+			result[substr] = (char *)malloc((i - start + 1) * sizeof(char));
+			if (result[substr] == NULL)
+				return ;
+			ft_strlcpy(result[substr], (s + start), (i - start + 1));
+			substr++;
+		}
+		else
+			i++;
+	}
 }
 
 char	**ft_split(char const *s, char c)
@@ -48,7 +64,6 @@ char	**ft_split(char const *s, char c)
 	int		word;
 	char	**result;
 	size_t	i;
-	size_t	size;
 
 	i = 0;
 	if (s == NULL)
@@ -57,7 +72,20 @@ char	**ft_split(char const *s, char c)
 	result = (char **)malloc((word + 1) * sizeof(char *));
 	if (result == NULL)
 		return (NULL);
-
-	result[--j] = NULL;
+	ft_malloc_words(s, c, result);
+	result[word] = NULL;
 	return (result);
 }
+
+// #include <stdio.h>
+// int main()
+// {
+// 	char teste[] = "ABACATE BATATA LIMAO";
+// 	char **result = ft_split(teste, ' ');
+// 	int i = 0;
+// 	while (result[i])
+// 	{
+// 		printf("%d %s \n", i, result[i]);
+// 		i++;
+// 	}
+// }
